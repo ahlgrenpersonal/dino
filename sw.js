8fcce6cfa4786e219db985ff50ff70877b90dabe
Repository Owner <ahlgrenpersonal@users.noreply.github.com>
2026-06-06 18:18:19 -0400

    Keep club swing anchored

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "dino-v25";
+const CACHE_NAME = "dino-v26";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=world6",
-  "./app.js?v=world6",
+  "./styles.css?v=world7",
+  "./app.js?v=world7",
   "./manifest.webmanifest",
   "./assets/icon.svg",
   "./assets/icon-192.png",
